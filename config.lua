7c43809f96b6afbc87eb326665d8e5d50d7ed51b
fabianosobrinho <fabiano_sobrinho@yahoo.com.br>
2017-07-06 17:12:00 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,8 +3,8 @@ local module = {}
 module.SSID = {}
 module.HOST = "m13.cloudmqtt.com"
 module.PORT = 14007
-module.USERNAME = "uloqgwuw"
-module.PASSWORD = "ckP7SbXZV1LK"
+module.USERNAME = "wqrtreterter"
+module.PASSWORD = "fghjgkjghjgfhfgh"
 module.ID = node.chipid()
 module.ENDPOINT = "/seu_endPoint/"..node.chipid()
 return module
